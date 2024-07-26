af73486151a2ef9bd32180f4c36128d150d2476a
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-07-26 15:49:18 +0300

    Catalog filters temporary styles added

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,16 +1,29 @@
 import Button from '../Button/Button';
+import css from './Filters.module.css';
 
 const Filters = () => {
   return (
     <section>
-      <label htmlFor="location">Location</label>
-      <input type="text" name="location" placeholder=""></input>
+      <div className={css.filtersContainer}>
+        <div className={css.filtersLocationDiv}>
+          <label className={css.filtersLabel} htmlFor="location">
+            Location
+          </label>
+          <input
+            className={css.filtersInput}
+            type="text"
+            name="location"
+            placeholder="City"
+          ></input>
+        </div>
 
-      <p>Filters</p>
-      <h4>Vehicle equipment</h4>
-      <h4>Vehicle type</h4>
-
-      <Button>Search</Button>
+        <p className={css.filtersSupportingText}>Filters</p>
+        <h4 className={css.filterCardsHeader}>Vehicle equipment</h4>
+        <div className={css.filterCards}>equipments</div>
+        <h4 className={css.filterCardsHeader}>Vehicle type</h4>
+        <div className={css.filterCards}>types</div>
+        <Button>Search</Button>
+      </div>
     </section>
   );
 };
