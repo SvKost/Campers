70653bcd6d56393a932ba01daf94586451175651
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-08-03 17:42:35 +0300

    CamperModal info added, styles updated

--- src/components/CamperModal/CamperModal.jsx
+++ src/components/CamperModal/CamperModal.jsx
@@ -3,48 +3,46 @@ import Icon from '../Icon/Icon';
 import css from './CamperModal.module.css';
 
 const CamperModal = ({ data, onClose }) => {
-  const { name, price, gallery } = data;
+  const { name, price, gallery, description } = data;
 
   return (
     <div className={css.modalWrapper}>
-      <div className={css.modalHeader}>
-        <HeaderLocation data={data} />
-        <p>€{price}</p>
-      </div>
-      <button
-        className={css.buttonCloseModal}
-        type="button"
-        onClick={() => onClose()}
-      >
-        <Icon
-          iconName="cross"
-          width={32}
-          height={32}
-          styles={css.iconCloseModal}
-        ></Icon>
-      </button>
-
       <div className={css.modalContent}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid non
-          reiciendis consequuntur, magni suscipit tenetur dignissimos eaque!
-          Minima, reiciendis vitae commodi nulla voluptatem, saepe est molestias
-          temporibus repudiandae aspernatur incidunt quaerat perferendis
-          consectetur rerum nemo asperiores repellendus. Alias dolores,
-          exercitationem fuga ratione ab id debitis enim ea est perspiciatis
-          voluptatem.
-        </p>
-        <div>
-          {gallery.map((image, index) => {
-            <img
-              key={index}
-              src={image}
-              alt={name}
-              className={css.camperImage}
-              width={290}
-              height={310}
-            />;
-          })}
+        <div className={css.modalHeader}>
+          <HeaderLocation data={data} />
+        </div>
+        <p className={css.camperPrice}>€{price}</p>
+
+        <button
+          className={css.buttonCloseModal}
+          type="button"
+          onClick={() => onClose()}
+        >
+          <Icon
+            iconName="cross"
+            width={32}
+            height={32}
+            styles={css.iconCloseModal}
+          ></Icon>
+        </button>
+        <div className={css.camperDescription}>
+          <div className={css.gallery}>
+            {gallery.map((image, index) => {
+              <img
+                key={index}
+                src={image}
+                alt={`${name} ${index + 1}`}
+                className={css.camperImage}
+                width={290}
+                height={310}
+              />;
+            })}
+          </div>
+          <div className={css.descriptionText}>{description}</div>
+        </div>
+        <div className={css.additionalDescription}>
+          <div className={css.additionalHeader}>Features</div>
+          <div className={css.additionalHeader}>Reviews</div>
         </div>
       </div>
     </div>
